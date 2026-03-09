df44a65a356adec3d7de0ddf8bcb98d182b7f3f5
Lovie-Tonimba <jechonialovie@gmail.com>
2026-03-09 20:50:13 +0700

    menu kantin selesai

--- main.dart
+++ main.dart
@@ -1,5 +1,22 @@
 import 'dart:io';
+import 'menu.dart';
 
-void main(){
-  
+void main() {
+  while (true) {
+  daftarMenu();
+
+  stdout.write("Pilih menu (1-6): ");
+  String? input = stdin.readLineSync();
+
+    if (input == '1' || input == '2' || input == '3' || input == '4') {
+
+    } else if (input == '5') {
+
+    } else if (input == '6') {
+      print("Terima kasih sudah mampir yaa..");
+      break; 
+    } else {
+      print("Input tidak valid, masukkan angka 1 sampai 6!");
+    }
+  }
 }
---
+++ menu.dart
@@ -0,0 +1,11 @@
+Map<String, int> daftarHarga = {'1': 12000, '2': 15000, '3': 5000, '4': 6000};
+
+void daftarMenu() {
+  print("\n=== KANTIN KAMPUS ===");
+  print("1. Nasi Goreng       (Rp 12.000)");
+  print("2. Nasi Ayam Geprek  (Rp 15.000)");
+  print("3. Es Teh            (Rp 5.000)");
+  print("4. Es Jeruk          (Rp 6.000)");
+  print("5. Lihat Riwayat");
+  print("6. Keluar");
+}
